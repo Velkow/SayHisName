baeb05a8317a8508a2fafa2bf2f92e05837b3ad5
Velkow <maktoumreda@gmail.com>
2022-05-01 00:32:57 +0000

    Learn React

---
+++ src/js/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import SayHisName from './SayHisName';
+
+export default function App(){
+    return (
+        <SayHisName />
+    );
+}
---
+++ src/js/SayHisName.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function SayHisName(){
+    return (
+        <h1>His Name</h1>
+    );
+}
